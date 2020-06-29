7537527df19a5f8d69cb25b278f7f88f3e0ab833
srillia <srillia@163.com>
2020-06-29 12:05:52 +0800

    修改超时时间

--- servers/grpcclient/grpc_client.go
+++ servers/grpcclient/grpc_client.go
@@ -117,7 +117,7 @@ func GetUserList(server *models.Server) (userIds []string, err error) {
 	defer conn.Close()
 
 	c := protobuf.NewAccServerClient(conn)
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Millisecond)
 	defer cancel()
 
 	req := protobuf.GetUserListReq{}
